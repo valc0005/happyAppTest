63e16247df4ee8b25e78524354a9486e406bad66
mtemmar <mfa.temmar@gmail.com>
2019-09-03 13:06:33 +0200

    added button and pop-up

--- app/src/main/java/happyapp/main/MainActivity.java
+++ app/src/main/java/happyapp/main/MainActivity.java
@@ -1,5 +1,9 @@
 package happyapp.main;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the button
+        Button btn = (Button) findViewById(R.id.btnMakeMeHappy);
+        // ... see what happens when the users clicks
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("my happy app", "the button work [log message] - happy app");
+                Toast.makeText(getApplicationContext(), "Blij!!!!!!!!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 }
+
