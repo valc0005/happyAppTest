5553b95669e3347ef784bc69c6285b0cb2e992c8
maas0089 <stijn_maas@live.nl>
2019-09-05 13:58:04 +0200

    Add dice game

--- app/src/main/java/happyapp/main/MainActivity.java
+++ app/src/main/java/happyapp/main/MainActivity.java
@@ -1,11 +1,14 @@
 package happyapp.main;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView text;
     private Button button;
+    private Button diceButton;
     private ImageView image;
 
     private final Typeface bold;
@@ -60,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
         text = findViewById(R.id.topText);
         button = findViewById(R.id.smileyButton);
+        diceButton = findViewById(R.id.diceButton);
         image = findViewById(R.id.happyImage);
     }
 
@@ -82,6 +87,50 @@ public class MainActivity extends AppCompatActivity {
         button.setTypeface(fontList.get(MathHelper.getRandomInt(0, fontList.size())));
         button.setTextColor(buttonTextColor);
         button.setBackgroundColor(buttonBackgroundColor);
+
+        int diceButtonTextColor = colorList[MathHelper.getRandomInt(0, colorList.length)];
+        int diceButtonBackgroundColor = colorList[MathHelper.getRandomInt(0, colorList.length)];
+
+        while (diceButtonTextColor == diceButtonBackgroundColor) {
+            diceButtonBackgroundColor = colorList[MathHelper.getRandomInt(0, colorList.length)];
         }
+
+        diceButton.setTypeface(fontList.get(MathHelper.getRandomInt(0, fontList.size())));
+        diceButton.setTextColor(diceButtonTextColor);
+        diceButton.setBackgroundColor(diceButtonBackgroundColor);
+        }
+
+    public void showDiceNumber(final View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        // Get a Random integer
+        int randomNumber = MathHelper.getRandomInt(1, 7);
+        // If random number is 6
+        if(randomNumber == 6) {
+            builder.setTitle("You won!")
+                    .setMessage("You rolled: " + randomNumber)
+                    .setCancelable(true)
+                    .setPositiveButton("Play new game", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialoginterface, int i) {
+                            showDiceNumber(view);
+                        }
+                    }).show();
+            // Creating dialog box
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            // Or else
+        } else {
+            builder.setTitle("The dice have been cast...")
+                    .setMessage("You rolled: " + randomNumber + ". " + "Pass the device on to the next player...")
+                    .setCancelable(true)
+                    .setPositiveButton("Re-roll", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialoginterface, int i) {
+                            showDiceNumber(view);
+                        }
+                    }).show();
+            // Creating dialog box
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
 }
 
--- app/src/main/java/happyapp/main/MathHelper.java
+++ app/src/main/java/happyapp/main/MathHelper.java
@@ -3,6 +3,6 @@ package happyapp.main;
 public class MathHelper {
 
     public static int getRandomInt(int min, int max) {
-        return (int) Math.floor(Math.random() * (max - min) + min);
+        return (int) (Math.random() * (max - min) + min);
     }
 }
